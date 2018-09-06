bbe0847096b228b891fe2b1f8906571ecf632ed8
Anna Gromych <anna_gromych_tp@bmc.com>
2018-09-06 16:55:38 +0300

    [basic assignment] Components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,27 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
-  serverName = 'TestServer';
+
+  serverName = '';
+  disableClicking = this.serverName === '';
 
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
   }
 
   ngOnInit() {
   }
 
-  onCreateServer() {
-    this.serverCreationStatus = 'server was created! The name is ' + this.serverName;
+  updateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    this.disableClicking = this.serverName === '';
   }
 
-  // onUpdateServerName(event: Event) {
-  onUpdateServerName(event: any) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-    // this.serverName = event.target.value; // in case of commented function declaration
+  resetInput() {
+    this.serverName = '';
+    this.disableClicking = true;
   }
 
 }
