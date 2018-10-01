d0520a253996b469448d7d89f9b321e485756341
Anna Gromych <anna_gromych_tp@bmc.com>
2018-10-01 10:25:12 +0300

    [3 basic assignment] remove unnecessary code

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,29 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
-  serverCreated = false;
-  servers = ['TestServer 1', 'TestServer 2'];
 
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
   }
 
   ngOnInit() {
-  }
-
-  onCreateServer() {
-    this.serverCreated = true;
-    this.servers.push(this.serverName);
-    this.serverCreationStatus = 'server was created! The name is ' + this.serverName;
-  }
-
-  onUpdateServerName(event: any) {
-    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
